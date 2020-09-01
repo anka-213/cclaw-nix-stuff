d96ef5092933cdd92eba3619eeccd52f63221b54
Andreas Källberg <anka.213@gmail.com>
2020-09-01 18:45:07 +0200

    Update other sha256 for gf-core

--- gf-core-overlay.nix
+++ gf-core-overlay.nix
@@ -9,7 +9,7 @@
       # Insert the desired all-hies commit here
       url = "https://github.com/anka-213/gf-core/archive/nix-support.tar.gz";
       # Insert the correct hash after the first evaluation
-      sha256 = "1gxaxbkb98xl4mlxs9i8sykcgyvjw2zv3l9whzvzvxwwhwj2jn0k";
+      sha256 = "0bwfb3zhn7i33rs90sddqccmq04hghjm0hlqqh6dmq5k5n7k8n6g";
     };
     in
     {
