ce0826f87a7eb1fbd0af555cd4809e819c0b2b9f
Andreas Källberg <anka.213@gmail.com>
2020-09-01 16:13:45 +0200

    Fix dependenciec

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { nixpkgs ? import <nixpkgs> {} }:
 
-{
+# Todo: use some better overlay pattern instead
+rec {
   gf-rgl = import ./build-gf-rgl.nix {};
   bnfc = import ./install-bnfc.nix {};
   gf = (import ./install-bnfc.nix {}).gf;
