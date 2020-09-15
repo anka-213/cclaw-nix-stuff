673e9a9a1ade0552888dff1376fd825665cabe14
Andreas Källberg <anka.213@gmail.com>
2020-09-15 20:52:50 +0200

    Quick and dirty fix for rgl encoding problems

--- gf-rgl.nix
+++ gf-rgl.nix
@@ -3,13 +3,15 @@
 stdenv.mkDerivation {
   name = "gf-rgl";
   # src = ./.;
-  buildInputs = [ghc gf];
+  buildInputs = [ ghc gf ];
   # doc requirements: [graphviz-nox]
   src = sources.gf-rgl;
   buildPhase = ''
+    export LC_ALL="C.UTF-8"
     runghc Setup.hs build
   '';
   installPhase = ''
+    export LC_ALL="C.UTF-8"
     mkdir -p $out/rgl
     runghc Setup.hs copy --dest=$out/rgl
   '';
@@ -22,14 +24,12 @@ stdenv.mkDerivation {
 # default: build copy
 
 # build: src/*/*.gf
-# 	$(RUNMAKE) build
+#   $(RUNMAKE) build
 
 # copy:
-# 	$(RUNMAKE) copy
+#   $(RUNMAKE) copy
 
 # install: build copy
 
 # doc: build
-# 	make -C doc GF_LIB_PATH=../dist
-
-
+#   make -C doc GF_LIB_PATH=../dist
