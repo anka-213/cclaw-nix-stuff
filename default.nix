e786a13b2bf0945cb2fb8f77ecc4cdef59f3a82b
Andreas Källberg <anka.213@gmail.com>
2020-09-18 10:56:56 +0200

    Use static binaries for bnfc

--- default.nix
+++ default.nix
@@ -1,16 +1,25 @@
 { sources ? import nix/sources.nix }:
 
 let
-  newpkgs = import sources.nixpkgs { overlays = [ (nixpkgs: pkgssuper: {
-    inherit (import ./gf-core.nix {inherit nixpkgs sources; }) gf;
-    # gf-rgl = import ./build-gf-rgl.nix {inherit nixpkgs ; };
-    gf-rgl = nixpkgs.callPackage ./gf-rgl.nix { inherit sources; };
-    bnfc = nixpkgs.haskellPackages.callCabal2nix "bnfc" (sources.bnfc + "/source") {};
-  } ) ]; };
+  newpkgs = import sources.nixpkgs {
+    overlays = [
+      (
+        nixpkgs: pkgssuper: {
+          # inherit (import ./gf-core.nix {inherit nixpkgs sources; }) gf;
+          gf = pkgssuper.haskell.lib.justStaticExecutables (import sources.gf-core { inherit nixpkgs; });
+          # gf-rgl = import ./build-gf-rgl.nix {inherit nixpkgs ; };
+          gf-rgl = nixpkgs.callPackage ./gf-rgl.nix { inherit sources; };
+          bnfc = pkgssuper.haskell.lib.justStaticExecutables (
+            nixpkgs.haskellPackages.callCabal2nix "bnfc" (sources.bnfc + "/source") {}
+          );
+        }
+      )
+    ];
+  };
 in
-  {
-    inherit (newpkgs) gf gf-rgl bnfc;
-  }
+{
+  inherit (newpkgs) gf gf-rgl bnfc;
+}
 
 # # Todo: use some better overlay pattern instead
 # let
