3ee32319ff8d5a493d394360181ce2282814b9e7
Andreas Källberg <anka.213@gmail.com>
2022-05-03 20:55:19 +0800

    Use a local copy of the patch

--- gf/overlay.nix
+++ gf/overlay.nix
@@ -10,12 +10,13 @@ final: prev: {
           _old: {
             # Fix utf8 encoding problems
             patches = [
-              (
-                prev.fetchpatch {
-                  url = "https://github.com/anka-213/gf-core/commit/6f1ca05fddbcbc860898ddf10a557b513dfafc18.patch";
-                  sha256 = "17vn3hncxm1dwbgpfmrl6gk6wljz3r28j191lpv5zx741pmzgbnm";
-                }
-              )
+              # (
+              #   prev.fetchpatch {
+              #     url = "https://github.com/anka-213/gf-core/commit/6f1ca05fddbcbc860898ddf10a557b513dfafc18.patch";
+              #     sha256 = "17vn3hncxm1dwbgpfmrl6gk6wljz3r28j191lpv5zx741pmzgbnm";
+              #   }
+              # )
+              ./encoding-fix.patch
             ];
             configureFlags = "-f c-runtime";
             # jailbreak = true; # jailbreak dependecies
